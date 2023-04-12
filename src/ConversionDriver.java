347780f1f533a919fad0d4fe750b3b1381f96c95
BrenoFlag <117334417+BrenoFlag@users.noreply.github.com>
2023-04-12 01:27:01 -0400

    Update ConversionDriver.java

--- src/ConversionDriver.java
+++ src/ConversionDriver.java
@@ -7,10 +7,10 @@ class ConversionDriver {
 
         do {
             System.out.println("What would you like to do?");
-            System.out.println("1) Temp convert");
+            System.out.println("1) Temperature convert");
             System.out.println("2) Weight convert");
             System.out.println("3) Currency convert");
-            System.out.println("4) Distance convert");
+            System.out.println("4) Time convert");
             System.out.println("5) Quit");
             System.out.print("Choice: ");
             choice = sc.nextInt();
